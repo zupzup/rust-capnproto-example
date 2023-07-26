14f18cf7304a4bfaa3cd8ffb267da7eb1512a562
Mario Zupan <mario@zupzup.org>
2023-07-26 21:48:47 +0200

    progress

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,127 @@ use capnp::{
     message::{Builder, ReaderOptions},
     serialize,
 };
-use std::fs;
+use serde::{Deserialize, Serialize};
+use std::{env, fs};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
 mod cats_capnp;
 
-fn main() {
-    // TODO: build server that broadcasts events, build client
-    println!("Hello, world!");
+#[derive(Serialize, Deserialize, Debug)]
+struct Cat {
+    name: String,
+    age: u8,
+    color: String,
+    cuteness: f32,
+    image: Option<Vec<u8>>,
+}
+// TODO: add nested object
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let img = read_img();
     let data = build_msg(&img);
     deserialize(&data);
+
+    let json_data = build_msg_json(&img);
+    deserialize_json(&json_data);
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        match &args[1][..] {
+            "c" => return client(&data, &json_data).await,
+            "s" => return server().await,
+            _ => (),
+        }
+    }
+
+    Ok(())
+}
+
+async fn client(data: &[u8], json_data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    println!("started in CLIENT mode");
+
+    let mut stream = TcpStream::connect("127.0.0.1:3000").await?;
+    stream.write_all(data).await?;
+
+    let mut stream = TcpStream::connect("127.0.0.1:3000").await?;
+    stream.write_all(json_data).await?;
+
+    Ok(())
+}
+
+async fn server() -> Result<(), Box<dyn std::error::Error>> {
+    println!("started in SERVER mode");
+    let listener = TcpListener::bind("127.0.0.1:3000").await?;
+
+    println!("server running at 127.0.0.1:3000");
+
+    loop {
+        match listener.accept().await {
+            Ok((mut socket, addr)) => {
+                tokio::spawn(async move {
+                    println!("accepted connection from: {}", addr);
+                    let mut buf = [0; 1024];
+                    let mut sum = 0;
+
+                    loop {
+                        let n = match socket.read(&mut buf).await {
+                            Ok(n) => {
+                                if n == 0 {
+                                    println!("read {:?} bytes", sum);
+                                    return;
+                                } else {
+                                    n
+                                }
+                            }
+                            Err(e) => {
+                                println!("error reading from socket: {}", e);
+                                return;
+                            }
+                        };
+                        sum += n;
+                    }
+                });
+            }
+            Err(e) => println!("error on client connection: {}", e),
+        }
+    }
 }
 
 fn read_img() -> Vec<u8> {
     fs::read("./minka.jpg").expect("can load image")
 }
 
-fn build_msg(img: &[u8]) -> Vec<u8> {
+// JSON
+// TODO: time
+fn build_msg_json(_img: &[u8]) -> Vec<u8> {
+    let cat = Cat {
+        name: String::from("Minka"),
+        age: 8,
+        color: String::from("lucky"),
+        cuteness: 100.0,
+        // image: Some(img.to_owned()),
+        image: None,
+    };
+
+    let data = serde_json::to_vec(&cat).expect("can json serialize cat");
+    println!("data: {:?}", data);
+    data
+}
+
+// TODO: time
+fn deserialize_json(data: &[u8]) {
+    let cat: Cat = serde_json::from_slice(data).expect("can deserialize json");
+    println!("json cat: {:?}", cat);
+}
+
+// CAPNPROTO
+
+// TODO: time
+fn build_msg(_img: &[u8]) -> Vec<u8> {
     let mut msg = Builder::new_default();
 
     let mut cat = msg.init_root::<cats_capnp::cat::Builder>();
@@ -33,6 +137,7 @@ fn build_msg(img: &[u8]) -> Vec<u8> {
     data
 }
 
+// TODO: time
 fn deserialize(data: &[u8]) {
     let reader = serialize::read_message(data, ReaderOptions::new()).expect("can create reader");
     let cat = reader
